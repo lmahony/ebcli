9d844d45ecda251e3c54cd9e316ec0d055ec5fe4
Larry Mahony <larry.mahony@teamaol.com>
2015-10-07 16:32:35 +0100

    Changing to directly hit weather underground api, cached at 30 minutes

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var NodeCache = require("node-cache");
 var fs = require('fs');
 var app = express();
 var currentWeather;
-var WEATHER = {
-	ttl: 300,
-	cacheKey: "weather"
-};
 var appCache = new NodeCache();
 //Read config values from a JSON file.
 var config = fs.readFileSync('./app_config.json', 'utf8');
@@ -25,16 +21,16 @@ app.get("/_healthcheck", function(req, res) {
 });
 
 app.get("/weather", function(req, res) {
-	currentWeather = appCache.get( WEATHER.cacheKey );
-	if ( currentWeather == undefined || !currentWeather.time){
+	currentWeather = appCache.get( config.WEATHER.CACHE_KEY );
+	if ( currentWeather == undefined || !currentWeather.current_observation){
 		console.log("No Cache. Making API response.");
 		request({
-			url: config.WEATHER_API,
+			url: config.WEATHER.WU_API,
 			json: true,
 		}, function( error, response, body) {
 			if (!error && response.statusCode === 200) {
 				currentWeather = body;
-				appCache.set(WEATHER.cacheKey, body, WEATHER.ttl);
+				appCache.set(config.WEATHER.CACHE_KEY, body, config.WEATHER.CACHE_TTL);
 				res.send(getWeatherDisplay());
 			} else {
 				console.log("Error retrieving json api code: %s , error: %s ", response.statusCode, error);
@@ -49,13 +45,16 @@ app.get("/weather", function(req, res) {
 
 var getWeatherDisplay = function() {
 	var cw = "";
-	if (currentWeather.time) {
-		cw += "Conds: " + currentWeather.lookslike + "<br/>";
-		cw += "Temp : " + currentWeather.temp_c + "c<br/>";
-		cw += "Feels: " + currentWeather.feelslike_c + "c<br/>";
-		cw += "Wind : " + currentWeather.wind + "<br/>";
-		cw += "Rain : " + currentWeather.precip + "<br/>";
-		cw += "Time : " + currentWeather.time + "<br/>";
+	var w = currentWeather.current_observation;
+	if (w) {
+		cw += "<img src='" + w.icon_url + "' alt='weather' />";
+		cw += "Conds: " + w.weather + "<br/>";
+		cw += "Temp : " + w.temp_c + "c<br/>";
+		cw += "Feels: " + w.feelslike_c + "c<br/>";
+		cw += "Wind : " + w.wind_string + "<br/>";
+		cw += "Rain : " + w.precip_1hr_metric + "<br/>";
+		cw += "Time : " + w.observation_time + "<br/>";
+		cw += "<br/><a href='" + w.forecast_url + "'>Click for Forecast</a>";
 	}
 	return cw;
 }
