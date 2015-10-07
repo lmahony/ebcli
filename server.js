315ce34ad4eca7f7f5d99b76c5119822ae4d10c2
Larry Mahony <larry.mahony@teamaol.com>
2015-10-07 09:50:20 +0100

    Trying to fix node error from aws

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+//app.use(express.logger('dev'));
+
 app.get("/", function(req, res) {
 	res.send("Hello ebcli test");
 });
@@ -9,8 +12,14 @@ app.get("/_healthcheck", function(req, res) {
 	res.send("APP OK");
 });
 
-var server = app.listen(80, function() {
+var server = app.listen(app.get('port'), function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("Example app listenting at http://%s:%s", host, port);
 });
+
+/*
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+*/
